1ae55e5d215df0fe8ce6d6a985ac1a1e320d81fd
muz-javed <muzammil.jvd@outlook.com>
2024-09-13 19:42:03 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,7 +84,7 @@ if (fs_pdf_file is not None) and (cov_pdf_file is not None):
     
     # Chat completion llm
     llm = ChatOpenAI(
-        model_name='gpt-3.5-turbo',
+        model_name='gpt-4',
         temperature=0.1
     )
     
