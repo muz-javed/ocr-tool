dda6169e45ff3b67336e60c1422ed8265c7e21f0
muz-javed <muzammil.jvd@outlook.com>
2024-09-14 18:14:45 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,109 +41,346 @@ with st.sidebar:
 
 
 
+fs_pdf_file = st.file_uploader('Upload Financial Statement', type = 'pdf')
 
-
-
-
-
-
-
-
-
-
-# Process the two PDF files
-cols = st.columns(2)
-with cols[0]:
-    fs_pdf_file = st.file_uploader('Upload Financial Statement', type = 'pdf')
-with cols[1]:
-    cov_pdf_file = st.file_uploader('Upload Covenants', type = 'pdf')
-
-
-if (fs_pdf_file is not None) and (cov_pdf_file is not None): 
-    pdf_files = [fs_pdf_file, cov_pdf_file]  # Update with actual file paths
-    raw_text = extract_text_from_pdfs(pdf_files)
-    
-    
-    # Split the extracted text into chunks
-    text_splitter = CharacterTextSplitter(
-        separator = "\n",
-        chunk_size = 1000,
-        chunk_overlap  = 200,
-        length_function = len,
-    )
-    chunks = text_splitter.split_text(raw_text)
-    
-    # Embed the text
-    embeddings = OpenAIEmbeddings(api_key=api_key)
-    VectorStore = FAISS.from_texts(chunks, embeddings)
-    
-    ##################Langchain Operations####################
-    retriever = VectorStore.as_retriever()
-    
-    # Chat completion llm
-    llm = ChatOpenAI(
-        model_name='gpt-4',
-        temperature=0.7
-    )
-    
-    # Conversational memory
-    conversational_memory = ConversationBufferMemory(
-        memory_key='chat_history',
-        chat_memory=FileChatMessageHistory('messages.json'),
-        return_messages=True
-    )
-    
-    # Retrieval QA chain
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=retriever,
-        callbacks=None
-    )
-    
-    # Tools
-    knowledge_tool = Tool(
-        name='Knowledge Base',
+reader = ocr_pdf_with_options(fs_pdf_file, './pdfservices-api-credentials.json')
+ 
+pdf_stream = BytesIO(reader)
+ 
+raw_text_financials = ''
+ 
+pdf_reader = PdfReader(pdf_stream)
+ 
+ 
+try:
+    pdf_reader = PdfReader(pdf_stream)
+ 
+    for i, page in enumerate(pdf_reader.pages):
+        text = page.extract_text()
+        if text:
+            raw_text_financials += text
+ 
+except Exception as e:
+    st.write(f"Error processing PDF: {e}")
+ 
+# st.write(raw_text)
+ 
+#Split the extracted text to chunks
+text_splitter = CharacterTextSplitter(
+    separator = "\n",
+    chunk_size = 1000,
+    chunk_overlap  = 200,
+    length_function = len,
+)
+chunks = text_splitter.split_text(raw_text_financials)
+ 
+#Embed the text
+embeddings = OpenAIEmbeddings()
+VectorStore = FAISS.from_texts(chunks, embeddings)
+store_name = "Stmts"
+ 
+# if os.path.exists(f"{store_name}.pkl"):
+#     with open(f"{store_name}.pkl", "rb") as f:
+#         VectorStore=pickle.load(f)
+# else:
+#     embeddings = OpenAIEmbeddings()
+#     VectorStore = FAISS.from_texts(chunks, embeddings)
+#     with open(f"{store_name}.pkl", "wb") as f:
+#         pickle.dump(VectorStore, f)
+ 
+##################Langchain Operations####################
+ 
+retriever = VectorStore.as_retriever()
+ 
+# chat completion llm
+llm = ChatOpenAI(
+    model_name='gpt-4',
+    temperature=0.7
+)
+# conversational memory
+conversational_memory = ConversationBufferMemory(
+    memory_key='chat_history',
+    return_messages=True
+)
+ 
+#Chat Prompt Template
+chat_prompt = ChatPromptTemplate(
+    input_variable=["input", "messages"],
+    messages=[
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad")
+    ]
+)
+ 
+# retrieval qa chain
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=retriever,
+    callbacks=None
+)
+ 
+ 
+#result = qa.invoke(query)
+#st.write(result)
+# Tools
+knowledge_tool = Tool(
+        name='KnowledgeBase',
         func=qa.run,
         description=(
             'use this tool when answering questions to get '
             'more information about the financial values'
         )
     )
+ 
+problem_chain = LLMMathChain.from_llm(llm=llm)
+math_tool = Tool.from_function(name="Calculator",
+                               func=problem_chain.run,
+                               description="Useful for when you need to answer numeric questions. This tool is "
+                                           "only for math questions and nothing else. Only input math "
+                                           "expressions, without text",
+                               )
+ 
+agent = initialize_agent(
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    prompt=chat_prompt,
+    tools=[knowledge_tool, math_tool],
+    llm=llm,
+    verbose=True,
+    #max_iterations=3,
+    #early_stopping_method='generate',
+    memory=conversational_memory
+)
+ 
+#Prompts Output
+df=pd.DataFrame()
+query = "What is the name of the company? Return one word answer."
+result = agent({"input":query})
+#st.write(result)
+df['Company Name'] = [result["output"]]
+ 
+query = "What is the latest date of the financial statement? Return one word answer"
+result = agent({"input":query})
+#st.write(result)
+df['As of Date'] = [result["output"]]
+ 
+query = "What are the latest current assets as of this date? Return one word answer"
+result = agent({"input":query})
+df['Current Assets'] = [result["output"]]
+#st.write(result)
+ 
+query = "What are the latest current liabilities as of the financial statement? Return one word answer"
+result = agent({"input":query})
+df['Current Liabilities'] = [result["output"]]
+#st.write(result)
+ 
+current_assets = df['Current Assets'].iloc[0]
+current_liabilities = df['Current Liabilities'].iloc[0]
+query = f"The current assets are {current_assets} and the current liabilities are {current_liabilities}. What is the current ratio? Return one number onlyr"
+result = agent({"input":query})
+df['Current Ratio'] = [result["output"]]
+#st.write(result)
+ 
+query = "What is the latest total Tangible Net worth as of the financial statement? Return one word answer"
+result = agent({"input":query})
+df['Net Worth'] = [result["output"]]
+#st.write(result)
+ 
+query = "What is the latest EBIDTA to Interest Expense + CPLTD as of the financial statement? Return one word answer"
+result = agent({"input":query})
+df['EBIDTA/Int+CPLTD'] = [result["output"]]
+#st.write(result)
+ 
+query = "What is the latest Debt to Net worth as of the financial statement? Return one word answer"
+result = agent({"input":query})
+df['Debt/Net Worth'] = [result["output"]]
+#st.write(result)
+ 
+st.write(df)
 
-    # covenant_tool = Tool(
-    #     name='Covenant Base',
-    #     func=qa.run,
-    #     description=(
-    #         'use this tool when answering questions to get '
-    #         'more information about the covenants and their thresholds'
-    #     )
-    # )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Process the two PDF files
+# cols = st.columns(2)
+# with cols[0]:
+#     fs_pdf_file = st.file_uploader('Upload Financial Statement', type = 'pdf')
+# with cols[1]:
+#     cov_pdf_file = st.file_uploader('Upload Covenants', type = 'pdf')
+
+
+# if (fs_pdf_file is not None) and (cov_pdf_file is not None): 
+#     pdf_files = [fs_pdf_file, cov_pdf_file]  # Update with actual file paths
+#     raw_text = extract_text_from_pdfs(pdf_files)
     
-    problem_chain = LLMMathChain.from_llm(llm=llm)
-    math_tool = Tool.from_function(name="Calculator",
-                                   func=problem_chain.run,
-                                   description="Useful for when you need to answer numeric questions. This tool is "
-                                               "only for math questions and nothing else. Only input math "
-                                               "expressions, without text",
-                                   )
     
-    # Initialize the agent
-    agent = initialize_agent(
-        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-        tools=[knowledge_tool, math_tool],
-        llm=llm,
-        verbose=True,
-        # max_iterations=3,
-        early_stopping_method='generate',
-        memory=conversational_memory
-    )
+#     # Split the extracted text into chunks
+#     text_splitter = CharacterTextSplitter(
+#         separator = "\n",
+#         chunk_size = 1000,
+#         chunk_overlap  = 200,
+#         length_function = len,
+#     )
+#     chunks = text_splitter.split_text(raw_text)
     
-    # Streamlit interface to ask a query
-    query_input = st.text_input("Ask a question", value="")
-    if query_input:
-        result = agent.run(query_input)
-        st.write(result)
+#     # Embed the text
+#     embeddings = OpenAIEmbeddings(api_key=api_key)
+#     VectorStore = FAISS.from_texts(chunks, embeddings)
+    
+#     ##################Langchain Operations####################
+#     retriever = VectorStore.as_retriever()
+    
+#     # Chat completion llm
+#     llm = ChatOpenAI(
+#         model_name='gpt-4',
+#         temperature=0.7
+#     )
+    
+#     # Conversational memory
+#     conversational_memory = ConversationBufferMemory(
+#         memory_key='chat_history',
+#         chat_memory=FileChatMessageHistory('messages.json'),
+#         return_messages=True
+#     )
+    
+#     # Retrieval QA chain
+#     qa = RetrievalQA.from_chain_type(
+#         llm=llm,
+#         chain_type="stuff",
+#         retriever=retriever,
+#         callbacks=None
+#     )
+    
+#     # Tools
+#     knowledge_tool = Tool(
+#         name='Knowledge Base',
+#         func=qa.run,
+#         description=(
+#             'use this tool when answering questions to get '
+#             'more information about the financial values'
+#         )
+#     )
+
+#     # covenant_tool = Tool(
+#     #     name='Covenant Base',
+#     #     func=qa.run,
+#     #     description=(
+#     #         'use this tool when answering questions to get '
+#     #         'more information about the covenants and their thresholds'
+#     #     )
+#     # )
+    
+#     problem_chain = LLMMathChain.from_llm(llm=llm)
+#     math_tool = Tool.from_function(name="Calculator",
+#                                    func=problem_chain.run,
+#                                    description="Useful for when you need to answer numeric questions. This tool is "
+#                                                "only for math questions and nothing else. Only input math "
+#                                                "expressions, without text",
+#                                    )
+    
+#     # Initialize the agent
+#     agent = initialize_agent(
+#         agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+#         tools=[knowledge_tool, math_tool],
+#         llm=llm,
+#         verbose=True,
+#         # max_iterations=3,
+#         early_stopping_method='generate',
+#         memory=conversational_memory
+#     )
+    
+#     # Streamlit interface to ask a query
+#     query_input = st.text_input("Ask a question", value="")
+#     if query_input:
+#         result = agent.run(query_input)
+#         st.write(result)
 
 
 
@@ -209,7 +446,7 @@ if (fs_pdf_file is not None) and (cov_pdf_file is not None):
 #     )
     
 #     #result = qa.invoke(query)
-#     #print(result)
+#     #st.write(result)
 #     knowledge_tool = Tool(
 #             name='Knowledge Base',
 #             func=qa.run,
