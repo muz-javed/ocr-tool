337a0862f02c277afab534045d4e5fe64475ff1d
muz-javed <muzammil.jvd@outlook.com>
2024-09-14 18:35:01 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,8 +95,8 @@ def ocr_pdf_with_options(input_pdf_path: str, credentials_path: str):
 
 
 with st.sidebar:
-    st.markdown(f"""<div><h3 style="text-align:left; color: white; ">LLM Chat App</h3></div>""", unsafe_allow_html=True)
-    st.markdown(f"""<div><h5 style="text-align:left; color: white; ">About</h5></div>""", unsafe_allow_html=True)
+    st.markdown(f"""<div><h2 style="text-align:left; color: white; ">LLM Chat App</h2></div>""", unsafe_allow_html=True)
+    st.markdown(f"""<div><h3 style="text-align:left; color: white; ">About</h3></div>""", unsafe_allow_html=True)
 
 
 
