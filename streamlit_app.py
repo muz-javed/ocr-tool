d21ea41314f84db71c268f811e66e5a56473a2fb
muz-javed <muzammil.jvd@outlook.com>
2024-09-15 19:20:41 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,283 +99,295 @@ with cols[1]:
 
 
 if (fs_pdf_file is not None) and (covenants_pdf_file is not None):
- reader = ocr_pdf_with_options(fs_pdf_file, './pdfservices-api-credentials.json')
-  
- pdf_stream = BytesIO(reader)
- raw_text_financials = ''
- pdf_reader = PdfReader(pdf_stream)
+
+ with st.spinner('Data is being processed...'):
  
- try:
-     pdf_reader = PdfReader(pdf_stream)
-  
-     for i, page in enumerate(pdf_reader.pages):
-         text = page.extract_text()
-         if text:
-             raw_text_financials += text
-  
- except Exception as e:
-     st.write(f"Error processing PDF: {e}")
-  
- # st.write(raw_text)
-  
- #Split the extracted text to chunks
- text_splitter = CharacterTextSplitter(
-     separator = "\n",
-     chunk_size = 700,
-     chunk_overlap  = 150,
-     length_function = len,
- )
- chunks = text_splitter.split_text(raw_text_financials)
-  
- #Embed the text
- embeddings = OpenAIEmbeddings()
- VectorStore = FAISS.from_texts(chunks, embeddings)
- store_name = "Stmts"
-  
- # if os.path.exists(f"{store_name}.pkl"):
- #     with open(f"{store_name}.pkl", "rb") as f:
- #         VectorStore=pickle.load(f)
- # else:
- #     embeddings = OpenAIEmbeddings()
- #     VectorStore = FAISS.from_texts(chunks, embeddings)
- #     with open(f"{store_name}.pkl", "wb") as f:
- #         pickle.dump(VectorStore, f)
-  
- ##################Langchain Operations####################
-  
- retriever = VectorStore.as_retriever()
-  
- # chat completion llm
- llm = ChatOpenAI(
-     model_name='gpt-4o',
-     temperature=0.7
- )
- # conversational memory
- conversational_memory = ConversationBufferMemory(
-     memory_key='chat_history',
-     return_messages=True
- )
-  
- #Chat Prompt Template
- chat_prompt = ChatPromptTemplate(
-     input_variable=["input", "messages"],
-     messages=[
-         MessagesPlaceholder(variable_name="chat_history"),
-         HumanMessagePromptTemplate.from_template("{input}"),
-         MessagesPlaceholder(variable_name="agent_scratchpad")
-     ]
- )
-  
- # retrieval qa chain
- qa = RetrievalQA.from_chain_type(
-     llm=llm,
-     chain_type="stuff",
-     retriever=retriever,
-     callbacks=None
- )
-  
-  
- #result = qa.invoke(query)
- #st.write(result)
- # Tools
- knowledge_tool = Tool(
-         name='KnowledgeBase',
-         func=qa.run,
-         description=(
-             'use this tool when answering questions to get '
-             'more information about the financial values'
-         )
-     )
-  
- problem_chain = LLMMathChain.from_llm(llm=llm)
- math_tool = Tool.from_function(name="Calculator",
-                                func=problem_chain.run,
-                                description="Useful for when you need to answer numeric questions. This tool is "
-                                            "only for math questions and nothing else. Only input math "
-                                            "expressions, without text",
-                                )
-  
- agent = initialize_agent(
-     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-     prompt=chat_prompt,
-     tools=[knowledge_tool, math_tool],
-     llm=llm,
-     verbose=True,
-     #max_iterations=3,
-     #early_stopping_method='generate',
-     memory=conversational_memory
- )
-  
- #Prompts Output
- df=pd.DataFrame()
- query = "What is the name of the company? Return one word answer."
- result = agent({"input":query})
- #st.write(result)
- df['Company Name'] = [result["output"]]
-  
- query = "What is the latest date of the financial statement? Return only the date in the format DD/MM/YYYY"
- result = agent({"input":query})
- #st.write(result)
- df['As of Date'] = [result["output"]]
- as_of_date = [result["output"]][0]
-
- query = "Extract the scale or multiplier in which the values are written within the financial statement? Return one word answer"
- result = agent({"input":query})
- # st.write(result["output"])
- currency_scale = result["output"]
-  
- query = f"What are the total current assets in {currency_scale} as of {as_of_date}? Return one word answer"
- result = agent({"input":query})
- df['Current Assets'] = [result["output"]]
- #st.write(result)
-  
- query = f"What are the total current liabilities in {currency_scale} as of {as_of_date}? Return one word answer"
- result = agent({"input":query})
- df['Current Liabilities'] = [result["output"]]
- # st.write("What are the total current liabilities in {currency_scale} as of {as_of_date}? Return one word answer")
- # st.write(result)
-  
- current_assets = df['Current Assets'].iloc[0]
- current_liabilities = df['Current Liabilities'].iloc[0]
- query = f"The current assets are {current_assets} and the current liabilities are {current_liabilities}. What is the current ratio? Return one number only"
- result = agent({"input":query})
- df['Current Ratio'] = [result["output"]]
- #st.write(result)
-  
- query = f"What is the Tangible Net worth as of {as_of_date}? Provide the result in {currency_scale} and make sure to return one word answer only"
- result = agent({"input":query})
- df['Tangible Net Worth'] = [result["output"]]
- # st.write(result)
-  
- query = f"What is the latest EBIDTA to Interest Expense + CPLTD as of {as_of_date}? Return one word answer"
- result = agent({"input":query})
- df['EBITDA to Debt Service Coverage Ratio'] = [result["output"]]
- # st.write(result)
-  
- query = f"What is the latest Debt/Net worth ratio as of {as_of_date}? Return one word answer"
- result = agent({"input":query})
- df['Debt/Net Worth'] = [result["output"]]
- # st.write(result)
-
- ##########################COVENANTS#################
- cov_reader = ocr_pdf_with_options(covenants_pdf_file, './pdfservices-api-credentials.json')
- cov_pdf_stream = BytesIO(cov_reader)
- raw_text_cov = ''
- try:
-     pdf_reader_cov = PdfReader(cov_pdf_stream)
-     for i, page in enumerate(pdf_reader_cov.pages):
-         text = page.extract_text()
-         if text:
-             raw_text_cov += text
-  
- except Exception as e:
-     print(f"Error processing PDF: {e}")
-  
- #Split the extracted text to chunks
- cov_chunks = text_splitter.split_text(raw_text_cov)
-  
- #Embed the text
- CovVectorStore = FAISS.from_texts(cov_chunks, embeddings)
- cov_retriever = CovVectorStore.as_retriever()
-  
- #Financial retriever
- financial_description = df.iloc[0].to_string()
- #embeddings = OpenAIEmbeddings()
- #financial_embedding = embeddings.embed_documents([financial_description])
-  
- # Create metadata for the row
- metadata = [df.iloc[0].to_dict()]
-  
- # Create a FAISS vector store with the embedding and metadata
- financial_vector_store = FAISS.from_texts([financial_description], embeddings)
-  
- fin_retriever = financial_vector_store.as_retriever()
-  
- cov_qa = RetrievalQA.from_chain_type(
-     llm=llm,
-     chain_type="stuff",
-     retriever=cov_retriever,
-     callbacks=None
- )
-  
- financial_qa_chain = RetrievalQA.from_chain_type(
- llm=llm,
-     chain_type="stuff",
-     retriever=fin_retriever,
-     callbacks=None
- )
-  
- # Cov Tools
- cov_knowledge_tool = Tool(
-         name='CovenantKnowledgeBase',
-         func=cov_qa.run,
-         description="Use this tool to check for covenant details and thresholds."
- )
-  
- financial_tool = Tool(
-     name="FinancialChecker",
-     func=financial_qa_chain.run,
-     description="Use this tool to retrieve financial information about the company."
- )
-  
- cov_agent = initialize_agent(
-     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-     prompt=chat_prompt,
-     tools=[cov_knowledge_tool, financial_tool, math_tool],
-     llm=llm,
-     verbose=True,
-     #max_iterations=3,
-     #early_stopping_method='generate',
-     memory=conversational_memory
- )
-  
- query = f"Check if company {df['Company Name'].iloc[0]} has breached any covenants based on the financial data provided, start your response with either 'Yes' or 'No'. Be detailed in your response."
-  
- # Run the agent using the tools
- result = cov_agent.run(query)
-
- if result[:3] == 'Yes':
-  df['Financial Covenants Flag'] = 1
-
- if result[:2] == 'No':
-  df['Financial Covenants Flag'] = 0
-
- df['Financial Covenants Breached Response'] = result
- 
- st.write(result)
-
- os.environ["TAVILY_API_KEY"] = "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"
-
- llm = ChatOpenAI(model_name="gpt-4o", temperature=0.7, openai_api_key = api_key)
- search = TavilySearchAPIWrapper()
- tavily_tool = TavilySearchResults(api_wrapper=search)
-
- agent_chain = initialize_agent(
-  [tavily_tool],
-  llm,
-  agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-  verbose=True,
-  )
-
- bankruptcy_response = agent_chain.run(
-    f"Is {df['Company Name'].iloc[0]} bankrupt currently? Start the response with either 'Yes' or 'No'.",
-)
- if bankruptcy_response[:3] == 'Yes':
-  df['Bankruptcy Flag'] = 1
-
- if bankruptcy_response[:2] == 'No':
-  df['Bankruptcy Flag'] = 0
-
- df['Bankruptcy Response'] = bankruptcy_response
-
-
-
+  reader = ocr_pdf_with_options(fs_pdf_file, './pdfservices-api-credentials.json')
    
- st.table(df)
+  pdf_stream = BytesIO(reader)
+  raw_text_financials = ''
+  pdf_reader = PdfReader(pdf_stream)
   
-
-
-
+  try:
+      pdf_reader = PdfReader(pdf_stream)
+   
+      for i, page in enumerate(pdf_reader.pages):
+          text = page.extract_text()
+          if text:
+              raw_text_financials += text
+   
+  except Exception as e:
+      st.write(f"Error processing PDF: {e}")
+   
+  # st.write(raw_text)
+   
+  #Split the extracted text to chunks
+  text_splitter = CharacterTextSplitter(
+      separator = "\n",
+      chunk_size = 700,
+      chunk_overlap  = 150,
+      length_function = len,
+  )
+  chunks = text_splitter.split_text(raw_text_financials)
+   
+  #Embed the text
+  embeddings = OpenAIEmbeddings()
+  VectorStore = FAISS.from_texts(chunks, embeddings)
+  store_name = "Stmts"
+   
+  # if os.path.exists(f"{store_name}.pkl"):
+  #     with open(f"{store_name}.pkl", "rb") as f:
+  #         VectorStore=pickle.load(f)
+  # else:
+  #     embeddings = OpenAIEmbeddings()
+  #     VectorStore = FAISS.from_texts(chunks, embeddings)
+  #     with open(f"{store_name}.pkl", "wb") as f:
+  #         pickle.dump(VectorStore, f)
+   
+  ##################Langchain Operations####################
+   
+  retriever = VectorStore.as_retriever()
+   
+  # chat completion llm
+  llm = ChatOpenAI(
+      model_name='gpt-4o',
+      temperature=0.7
+  )
+  # conversational memory
+  conversational_memory = ConversationBufferMemory(
+      memory_key='chat_history',
+      return_messages=True
+  )
+   
+  #Chat Prompt Template
+  chat_prompt = ChatPromptTemplate(
+      input_variable=["input", "messages"],
+      messages=[
+          MessagesPlaceholder(variable_name="chat_history"),
+          HumanMessagePromptTemplate.from_template("{input}"),
+          MessagesPlaceholder(variable_name="agent_scratchpad")
+      ]
+  )
+   
+  # retrieval qa chain
+  qa = RetrievalQA.from_chain_type(
+      llm=llm,
+      chain_type="stuff",
+      retriever=retriever,
+      callbacks=None
+  )
+   
+   
+  #result = qa.invoke(query)
+  #st.write(result)
+  # Tools
+  knowledge_tool = Tool(
+          name='KnowledgeBase',
+          func=qa.run,
+          description=(
+              'use this tool when answering questions to get '
+              'more information about the financial values'
+          )
+      )
+   
+  problem_chain = LLMMathChain.from_llm(llm=llm)
+  math_tool = Tool.from_function(name="Calculator",
+                                 func=problem_chain.run,
+                                 description="Useful for when you need to answer numeric questions. This tool is "
+                                             "only for math questions and nothing else. Only input math "
+                                             "expressions, without text",
+                                 )
+   
+  agent = initialize_agent(
+      agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+      prompt=chat_prompt,
+      tools=[knowledge_tool, math_tool],
+      llm=llm,
+      verbose=True,
+      #max_iterations=3,
+      #early_stopping_method='generate',
+      memory=conversational_memory
+  )
+   
+  #Prompts Output
+  df=pd.DataFrame()
+  query = "What is the name of the company? Return one word answer."
+  result = agent({"input":query})
+  #st.write(result)
+  df['Company Name'] = [result["output"]]
+   
+  query = "What is the latest date of the financial statement? Return only the date in the format DD/MM/YYYY"
+  result = agent({"input":query})
+  #st.write(result)
+  df['As of Date'] = [result["output"]]
+  as_of_date = [result["output"]][0]
+ 
+  query = "Extract the scale or multiplier in which the values are written within the financial statement? Return one word answer"
+  result = agent({"input":query})
+  # st.write(result["output"])
+  currency_scale = result["output"]
+   
+  query = f"What are the total current assets in {currency_scale} as of {as_of_date}? Return one word answer"
+  result = agent({"input":query})
+  df['Current Assets'] = [result["output"]]
+  #st.write(result)
+   
+  query = f"What are the total current liabilities in {currency_scale} as of {as_of_date}? Return one word answer"
+  result = agent({"input":query})
+  df['Current Liabilities'] = [result["output"]]
+  # st.write("What are the total current liabilities in {currency_scale} as of {as_of_date}? Return one word answer")
+  # st.write(result)
+   
+  current_assets = df['Current Assets'].iloc[0]
+  current_liabilities = df['Current Liabilities'].iloc[0]
+  query = f"The current assets are {current_assets} and the current liabilities are {current_liabilities}. What is the current ratio? Return one number only"
+  result = agent({"input":query})
+  df['Current Ratio'] = [result["output"]]
+  #st.write(result)
+   
+  query = f"What is the Tangible Net worth as of {as_of_date}? Provide the result in {currency_scale} and make sure to return one word answer only"
+  result = agent({"input":query})
+  df['Tangible Net Worth'] = [result["output"]]
+  # st.write(result)
+   
+  query = f"What is the latest EBIDTA to Interest Expense + CPLTD as of {as_of_date}? Return one word answer"
+  result = agent({"input":query})
+  df['EBITDA to Debt Service Coverage Ratio'] = [result["output"]]
+  # st.write(result)
+   
+  query = f"What is the latest Debt/Net worth ratio as of {as_of_date}? Return one word answer"
+  result = agent({"input":query})
+  df['Debt/Net Worth'] = [result["output"]]
+  # st.write(result)
+ 
+  ##########################COVENANTS#################
+  cov_reader = ocr_pdf_with_options(covenants_pdf_file, './pdfservices-api-credentials.json')
+  cov_pdf_stream = BytesIO(cov_reader)
+  raw_text_cov = ''
+  try:
+      pdf_reader_cov = PdfReader(cov_pdf_stream)
+      for i, page in enumerate(pdf_reader_cov.pages):
+          text = page.extract_text()
+          if text:
+              raw_text_cov += text
+   
+  except Exception as e:
+      print(f"Error processing PDF: {e}")
+   
+  #Split the extracted text to chunks
+  cov_chunks = text_splitter.split_text(raw_text_cov)
+   
+  #Embed the text
+  CovVectorStore = FAISS.from_texts(cov_chunks, embeddings)
+  cov_retriever = CovVectorStore.as_retriever()
+   
+  #Financial retriever
+  financial_description = df.iloc[0].to_string()
+  #embeddings = OpenAIEmbeddings()
+  #financial_embedding = embeddings.embed_documents([financial_description])
+   
+  # Create metadata for the row
+  metadata = [df.iloc[0].to_dict()]
+   
+  # Create a FAISS vector store with the embedding and metadata
+  financial_vector_store = FAISS.from_texts([financial_description], embeddings)
+   
+  fin_retriever = financial_vector_store.as_retriever()
+   
+  cov_qa = RetrievalQA.from_chain_type(
+      llm=llm,
+      chain_type="stuff",
+      retriever=cov_retriever,
+      callbacks=None
+  )
+   
+  financial_qa_chain = RetrievalQA.from_chain_type(
+  llm=llm,
+      chain_type="stuff",
+      retriever=fin_retriever,
+      callbacks=None
+  )
+   
+  # Cov Tools
+  cov_knowledge_tool = Tool(
+          name='CovenantKnowledgeBase',
+          func=cov_qa.run,
+          description="Use this tool to check for covenant details and thresholds."
+  )
+   
+  financial_tool = Tool(
+      name="FinancialChecker",
+      func=financial_qa_chain.run,
+      description="Use this tool to retrieve financial information about the company."
+  )
+   
+  cov_agent = initialize_agent(
+      agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+      prompt=chat_prompt,
+      tools=[cov_knowledge_tool, financial_tool, math_tool],
+      llm=llm,
+      verbose=True,
+      #max_iterations=3,
+      #early_stopping_method='generate',
+      memory=conversational_memory
+  )
+   
+  query = f"Check if company {df['Company Name'].iloc[0]} has breached any covenants based on the financial data provided, start your response with either 'Yes' or 'No'. Be detailed in your response."
+   
+  # Run the agent using the tools
+  result = cov_agent.run(query)
+ 
+  if result[:3] == 'Yes':
+   df['Financial Covenants Flag'] = 1
+ 
+  if result[:2] == 'No':
+   df['Financial Covenants Flag'] = 0
+ 
+  df['Financial Covenants Breached Response'] = result
+  
+  # st.write(result)
+ 
+  os.environ["TAVILY_API_KEY"] = "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"
+ 
+  llm = ChatOpenAI(model_name="gpt-4o", temperature=0.7, openai_api_key = api_key)
+  search = TavilySearchAPIWrapper()
+  tavily_tool = TavilySearchResults(api_wrapper=search)
+ 
+  agent_chain = initialize_agent(
+   [tavily_tool],
+   llm,
+   agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+   verbose=True,
+   )
+ 
+  bankruptcy_response = agent_chain.run(
+     f"Is {df['Company Name'].iloc[0]} bankrupt currently? Start the response with either 'Yes' or 'No'.",
+ )
+  if bankruptcy_response[:3] == 'Yes':
+   df['Bankruptcy Flag'] = 1
+ 
+  if bankruptcy_response[:2] == 'No':
+   df['Bankruptcy Flag'] = 0
+ 
+  df['Bankruptcy Response'] = bankruptcy_response
+    
+  st.markdown("""<div style='text-align: left; padding-left: 10px; color: #9cdea8; border-radius: 5px;'><p>Data has been processed successfully.</p></div>""", unsafe_allow_html=True)
+        
+   
+  buffer = BytesIO()
+  df.to_excel(buffer, index=False, engine='xlsxwriter')
+  buffer.seek(0)
+  b64 = base64.b64encode(buffer.read()).decode()
+  href = f'<a href="data:application/octet-stream;base64,{b64}" download="Raw Data.xlsx" style="text-decoration: none;">here</a>'
+  
+  st.markdown(f"""
+  <span style="color: white; padding-left: 10px; margin-top: -20px;">
+          Click {href} to download raw data.</p>
+          
+  </span>
+  """, unsafe_allow_html=True)
+ 
+ 
 
 
 
